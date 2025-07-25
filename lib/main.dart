4f911f9ebeb7138c1a49b693153aafe13bd2d330
AbhijithNellikkat <abhijithnellikkat@AbhijithNellikkats-MacBook-Air.local>
2025-07-25 16:18:08 +0530

    dev created

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lilac Chat',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFE6446E)),
       ),
       home: Scaffold(),
     );
